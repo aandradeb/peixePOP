48852d9eca099e82b4a974b04b1dddb602f9b695
acarneirod <92722108+acarneirod@users.noreply.github.com>
2021-11-23 13:12:32 +0100

    Añadidos métodos de API
    + getFacturas()
    + getLineasFacturas()
    + getLineasPedidos()
    + getPedidos()

--- acuario-maven/src/main/java/common/APIRequest.java
+++ acuario-maven/src/main/java/common/APIRequest.java
@@ -40,4 +40,68 @@ public class APIRequest {
         }
         return lista;
     }
+    
+    // API FACTURAS
+    public ArrayList<Factura> getFacturas(){
+        JSONArray jsonResponse = api.getResponse("/facturas");
+        ArrayList<Factura> lista=new ArrayList();
+        for (int indice = 0; indice < jsonResponse.length(); indice++) {
+            JSONObject jsonObject = jsonResponse.getJSONObject(indice);
+            String codigo = jsonObject.getString("codigo");
+            String codigo_cliente = jsonObject.getString("codigo_cliente");
+            String fecha = jsonObject.getString("fecha");
+            String importe_total = jsonObject.getString("importe_total");
+            String pagada = jsonObject.getString("pagada");
+            lista.add(new Factura(codigo,codigo_cliente,fecha,importe_total,pagada));
+        }
+        return lista;
+    }
+    
+    // API LINEAS FACTURAS
+    public ArrayList<Linea_Factura> getLineasFacturas(){
+        JSONArray jsonResponse = api.getResponse("/lineas_facturas");
+        ArrayList<Linea_Factura> lista=new ArrayList();
+        for (int indice = 0; indice < jsonResponse.length(); indice++) {
+            JSONObject jsonObject = jsonResponse.getJSONObject(indice);
+            String codigo = jsonObject.getString("codigo");
+            String cantidad = jsonObject.getString("cantidad");
+            String descripcion = jsonObject.getString("descripcion");
+            String precio = jsonObject.getString("precio");
+            String codigo_factura = jsonObject.getString("codigo_factura");
+            lista.add(new Linea_Factura(codigo,cantidad,descripcion,precio,codigo_factura));
+        }
+        return lista;
+    }
+    
+    // API LINEAS PEDIDOS
+    public ArrayList<Linea_Pedido> getLineasPedidos(){
+        JSONArray jsonResponse = api.getResponse("/lineas_pedidos");
+        ArrayList<Linea_Pedido> lista=new ArrayList();
+        for (int indice = 0; indice < jsonResponse.length(); indice++) {
+            JSONObject jsonObject = jsonResponse.getJSONObject(indice);
+            String codigo = jsonObject.getString("codigo");
+            String codigo_articulo = jsonObject.getString("codigo_articulo");
+            String cantidad = jsonObject.getString("cantidad");
+            String precio = jsonObject.getString("precio");
+            String codigo_pedido = jsonObject.getString("codigo_pedido");
+            lista.add(new Linea_Pedido(codigo,codigo_articulo,cantidad,precio,codigo_pedido));
+        }
+        return lista;
+    }
+    
+    // API PEDIDOS
+    public ArrayList<Pedido> getPedidos(){
+        JSONArray jsonResponse = api.getResponse("/pedidos");
+        ArrayList<Pedido> lista=new ArrayList();
+        for (int indice = 0; indice < jsonResponse.length(); indice++) {
+            JSONObject jsonObject = jsonResponse.getJSONObject(indice);
+            String codigo = jsonObject.getString("codigo");
+            String fecha = jsonObject.getString("fecha");
+            String recibido = jsonObject.getString("recibido");
+            lista.add(new Pedido(codigo,fecha,recibido));
+        }
+        return lista;
+    }
+    
+    
 }
